a476f3e48cadeefbb20a54e134f7146d81417b84
Jacob Mazur <jacobmazur@Jacobs-MacBook-Pro.local>
2021-02-11 14:40:02 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { data } from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quote: '',
+    };
+  }
+
+  handler = () => {
+    this.setState({
+      quote: data[Math.floor(Math.random() * data.length)],
+    });
+  };
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Force Quotes</h1>
+        </header>
+        <div className='container'>
+          <div className='joke'>
+            <p>{this.state.quote}</p>
+          </div>
+          <button onClick={this.handler}>
+            what does force have to say this time?🤔
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,75 @@
+export const data = [
+  'Boys shit is boutta get real weird at home. Just know y’all still my dawgs regeerrdless',
+
+  'The nectar needs not bringing...the nectar is already on the vessel ',
+
+  'just wait till we get onto east main, its gonna be literally dangerous',
+
+  'ohhh occhi you’re getting hyped against',
+
+  'digital dash',
+
+  'Ima raise the baby. You know ion fuck round w this mafs. That fetus be beautiful ima tell you that. That dark life don’t be me',
+
+  'facts thats nice',
+
+  'That’s an absurd amount of breaking bad porn',
+
+  'I’m tryna figure out if Gesmondi’s got beef',
+
+  'I hope living here is a LIVING HELL for you as an out of town sports fan',
+
+  'my sister has legit LIVED THERE for seven years bud. She’d laugh so hard at you',
+
+  'I just watched a boston sports team actually raise the surrender flag in front of my eyes',
+
+  'My ass you didnt',
+
+  '🧐🤔',
+
+  'Yeah sorry James whit missed a couple of games because his dad died you sick fuck',
+
+  'Occhi how does my taint taste?',
+
+  'I’ll kill ur families if u want',
+
+  'Now if I were you I’d stop feeding into my enjoyment',
+
+  'Yeah I revel in your anger',
+
+  'Love when broadcasters say gaping hole',
+
+  'Yeah well the pats are losing to the jets so my brain is pretty much a wasteland rn',
+
+  'What kind of cunt trades Alvin Kamara',
+
+  'Give me some warmup tosses and I’ll be Jakey Dimes',
+
+  'That’s why you’re a fucking PUSSY BRO',
+
+  'Tinder is a fuckin high school runion rn',
+
+  'People be mad fake in this town',
+
+  'That’s my point about cole beasley',
+
+  'Donavan Peoples-Jones is fucking ur bitch',
+
+  'Stop drinking his Kool aid',
+
+  'I’m shaming the choice because the guy was a sex king as it was',
+
+  'Mazur the People’s Jones',
+
+  'She might be 16 but them titties be 35',
+
+  'Wanna do something on Sunday afternoon?',
+
+  'No bitch because because the only pats game of the week is on Sunday like it is literally every single fucking week of the fucking season',
+
+  'Mazur has me injured rn',
+
+  'Wish we were rubbing at Abbate’s rn😢',
+
+  'I ain’t sleepin in a fitted🤣🤣🤣bruh that’s neck trouble',
+];
